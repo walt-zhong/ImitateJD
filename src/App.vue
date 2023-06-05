c3dfd1dd5fd038c8e6f3da022eb9740956b5d073
walt-zhong <1517446604@qq.com>
2023-06-05 08:07:46 +0800

    初始提交，完成底部导航栏和顶部的地址部分

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,93 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+ <div>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe7f1;</span>
+      北京联合大学北四环东路97号
+      北京联合大学北四环东路97号北京联合大学北四环东路97号北京联合大学北四环东路97号北京联合大学北四环东路97号北京联合大学北四环东路97号
+      <span class="iconfont position__notice">&#xe7c4;</span>
+    </div>
+  </div>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe6d2;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe6b1;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe612;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe7bd;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
+ </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import './style/viriable.scss';
+@import './style/mixins.scss';
+.wrapper{
+  position:absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right: 0;
+  padding: 0 .18rem;
 }
+.position{
+  position: relative;
+  @include ellipsis;
+  padding: .16rem .24rem .16rem 0;
+  line-height: .22rem;
+  font-size: .16rem;
+  color: $content-font-color;
+  .position__icon{
+    position: relative;
+    top: .01rem;
+    font-size: .2rem;
+  }
 
-nav {
-  padding: 30px;
+  .position__notice{
+    position: absolute;
+    right: 0;
+    top: .17rem;
+    font-size: .2rem;
+  }
+}
+.docker{
+  display: flex;
+  box-sizing: border-box;
+  padding: 0 .18rem;
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: .01rem solid #F1F1F1;
+  color:$content-font-color;
+  // docer 下的item元素
+  &__item{
+    flex: 1;
+  text-align: center;
+  .iconfont{
+    margin: .07rem 0 .02rem 0;
+    font-size: .18rem;
+  }
+  &--active{
+    color: #1FA4FC;
+  }
+  }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  &__title{
+    font-size: .2rem;
+    transform: scale(0.5,0.5);
+    transform-origin: center top;
   }
 }
 </style>
